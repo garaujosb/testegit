b4356f819b96ee2b703c8a53d17cf6a4e10aa8fe
garaujosb <55881061+garaujosb@users.noreply.github.com>
2019-11-08 08:35:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.status(200).send({ message: "ola mundo, estou funcionando"});
 })
 
+app.get('/ola', (req, res) => {
+  res.status(200).send({ message: "oi, eu sou a rota olá"});
+})
+
 app.listen(3001, () => {
   console.log('api rodando na porta 3001');
 })
